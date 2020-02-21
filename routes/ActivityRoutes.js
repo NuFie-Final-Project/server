b0adc256c5c0e9441eddc2325d12b4dd8c80a3c0
Carl <carlh3h@yahoo.co.id>
2020-02-21 21:37:18 +0700

    add activity endpoints for commit, invite, kick, join, and leave

--- routes/ActivityRoutes.js
+++ routes/ActivityRoutes.js
@@ -2,7 +2,9 @@ const router = require("express").Router();
 const ActivityController = require("../controllers/ActivityController.js");
 const {
     userAuthentication,
-    activityAuthorization
+    activityAuthorization,
+    activityMemberAuthorization,
+    activityPendingAuthorization
 } = require("../middlewares/auth");
 const { singleUpload } = require("../services/imageUpload");
 
@@ -16,6 +18,16 @@ router.patch(
     singleUpload,
     ActivityController.updateOne
 );
+router.post("/commit/:id", activityAuthorization, ActivityController.commit);
+router.post("/invite/:id", activityAuthorization, ActivityController.invite);
+router.post("/inviteAccept/:id", activityPendingAuthorization, ActivityController.inviteAccept);
+router.post("/inviteReject/:id", activityPendingAuthorization, ActivityController.inviteReject);
+router.post("/kick/:id", activityAuthorization, ActivityController.kick);
+router.post("/join/:id", ActivityController.join);
+router.post("/joinAccept/:id", activityAuthorization, ActivityController.joinAccept);
+router.post("/joinReject/:id", activityAuthorization, ActivityController.joinReject);
+router.post("/leave/:id", activityMemberAuthorization, ActivityController.leave);
+router.patch("/:id", activityAuthorization, ActivityController.updateOne);
 router.delete("/:id", activityAuthorization, ActivityController.deleteOne);
 
 module.exports = router;

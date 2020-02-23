f7724fcd9233dbc3a1d904f812b8f8f9b808f42f
zoe-zachary <zoezachary27@gmail.com>
2020-02-23 16:14:36 +0700

    Current user test is done, for the register and signin test need to wait till tomorrow. Added endpoint and controller getByInterest on ActivityRoutes.js

--- test/0-user.test.js
+++ test/0-user.test.js
@@ -1,110 +1,106 @@
-const chai = require("chai");
-const chaiHttp = require("chai-http");
+const chai = require('chai');
+const chaiHttp = require('chai-http');
 const expect = chai.expect;
 
-const User = require("../models/User.js");
-const Activity = require("../models/Activity.js");
+const User = require('../models/User.js');
+const Activity = require('../models/Activity.js');
 
-const app = require("../app");
-const {
-    removeAllUser,
-    createUser,
-    createActivity,
-    removeAllActivity
-} = require("./helpers");
+const app = require('../app');
+const { removeAllUser, createUser, createActivity, removeAllActivity } = require('./helpers');
 
 chai.use(chaiHttp);
 
 const activityCreatorUser = {
-    firstName: "creator",
-    email: "creator@mail.com",
-    password: "asd123"
+	firstName: 'creator',
+	email: 'creator@mail.com',
+	password: 'asd123'
 };
 
 const user1Data = {
-    firstName: "user1",
-    email: "user1@mail.com",
-    password: "asd123",
-    interests: ["javascript", "react"]
+	firstName: 'user1',
+	email: 'user1@mail.com',
+	password: 'asd123',
+	interests: [ 'javascript', 'react' ]
 };
 
 const user2Data = {
-    firstName: "user2",
-    email: "user2@mail.com",
-    password: "asd123",
-    interests: ["programming"]
+	firstName: 'user2',
+	email: 'user2@mail.com',
+	password: 'asd123',
+	interests: [ 'programming' ]
 };
 
 const activity1Data = {
-    title: "activity1",
-    description: "description1"
+	title: 'activity1',
+	description: 'description1'
 };
 
 let createdUser0, createdUser1, createdUser2;
-let createdActivity1;
+let activity1;
 
-describe.only("/user", function() {
-    before(async function() {
-        createdUser0 = await createUser(activityCreatorUser);
-        createdUser1 = await createUser(user1Data);
-        createdUser2 = await createUser(user2Data);
+describe('/user', function() {
+	before(async function() {
+		createdUser0 = await createUser(activityCreatorUser);
+		createdUser1 = await createUser(user1Data);
+		createdUser2 = await createUser(user2Data);
+	});
 
-        // createdActivity1 = await Activity.create({
-        //     owner: createdUser0._id,
-        //     title: activity1Data.title,
-        //     description: activity1Data.description,
-        //     tags: JSON.stringify(["react", "vue", "javascript"])
-        // });
+	after(async function() {
+		await removeAllUser();
+		await removeAllActivity();
+	});
 
-        // createdActivity1 = tempActivityResp.activity._id;
-    });
+	describe('Edit user profile: PATCH /users/:id', function() {
+		it('should return a modified user object - status code 200', async function() {
+			const updateData = {
+				lastName: 'Bond'
+			};
 
-    after(async function() {
-        await removeAllUser();
-        await removeAllActivity();
-    });
+			const response = await chai
+				.request(app)
+				.patch('/users')
+				.set({
+					token: createdUser1.token
+				})
+				.send(updateData);
 
-    describe("Edit user profile: PATCH /users/:id", function() {
-        it("should return a modified user object - status code 200", async function() {
-            const updateData = {
-                lastName: "Bond"
-            };
+			expect(response).to.have.status(200);
+			expect(response.body.user.lastName).to.equal('Bond');
+		});
+	});
 
-            const response = await chai
-                .request(app)
-                .patch("/users")
-                .set({
-                    token: createdUser1.token
-                })
-                .send(updateData);
+	describe('Get self detail: GET /users', function() {
+		it('Return a user object with the post populated - status 200', async function() {
+			const activityResp = await chai
+				.request(app)
+				.post('/activities')
+				.set({
+					token: createdUser0.token
+				})
+				.send(activity1Data);
 
-            expect(response).to.have.status(200);
-            expect(response.body.user.lastName).to.equal("Bond");
-        });
-    });
+			activity1 = activityResp.body.activity;
 
-    describe("Get self detail: GET /users/:id", function() {
-        it("Return a user object with the post populated - status 200", async function() {
-            const activityResp = await chai
-                .request(app)
-                .post("/activities")
-                .set({
-                    token: createdUser0.token
-                })
-                .send(activity1Data);
+			const response = await chai.request(app).get('/users').set({
+				token: createdUser0.token
+			});
 
-            const activity = activityResp.body.activity;
+			expect(response).to.have.status(200);
+			expect(response.body.user.posts[0]).to.equal(activity1._id);
+		});
+	});
 
-            const response = await chai
-                .request(app)
-                .get("/users")
-                .set({
-                    token: createdUser0.token
-                });
+	describe('Get user detail by id: GET /users/:id', function() {
+		it('Returns a user object with the id in the parameter - status 200', async function() {
+			const response = await chai.request(app).get(`/users/${createdUser0.user._id}`).set({
+				token: createdUser0.token
+			});
 
-            // console.log(response.body.user.posts[0], "isi body");
-            expect(response).to.have.status(200);
-            expect(response.body.user.posts[0]).to.equal(activity._id);
-        });
-    });
+			expect(response).to.have.status(200);
+			expect(response.body.user._id).to.equals(createdUser0.user._id.toString());
+			expect(response.body.user.posts[0]).to.equal(activity1._id);
+		});
+	});
+
+	describe('Get');
 });
